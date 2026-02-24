5c219116c43c705224ae7e6c7d4d00fcebd07052
한도윤 <92493882+semin98@users.noreply.github.com>
2026-02-24 16:02:07 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 708 ms, Memory: 135152 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		HashMap<Integer, String> map1 = new HashMap<>();
+		HashMap<String, Integer> map2 = new HashMap<>();
+		
+		for (int i = 1; i <= n; i++) {
+			String pocketmon = br.readLine();
+			
+			map1.put(i, pocketmon);
+			map2.put(pocketmon, i);
+		}
+		
+		for (int i = 0; i < m; i++) {
+			String input = br.readLine();
+			
+			if (input.matches("-?\\d+")) {
+			    sb.append(map1.get(Integer.parseInt(input))).append("\n");
+			} else {
+			    sb.append(map2.get(input)).append("\n");
+			}
+		}
+		
+		System.out.println(sb);
+	}
+}
