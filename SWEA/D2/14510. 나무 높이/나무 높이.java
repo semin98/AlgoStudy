e1f6fb2dd4f4e1a4a54bd6bed464d0eb546f07bf
한도윤 <92493882+semin98@users.noreply.github.com>
2026-02-25 22:40:00 +0900

    [D2] Title: 나무 높이, Time: 94 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -4,75 +4,56 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution {
-		
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st;
-		
-		for(int test_case = 1; test_case <= T; test_case++) {
-			
-			int N = Integer.parseInt(br.readLine());
-			int[] arr = new int[N];
-			st = new StringTokenizer(br.readLine());
-			int even_cnt = 0;
-			int odd_cnt = 0;
-			int ans = 0;
-			
-			int max = 0;
-			
-			for(int i = 0; i < N; i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
-				max = Math.max(max, arr[i]);
-				
-			}
-			
-			for(int i = 0; i < N; i++) {
-				int tmp = max - arr[i];
-				odd_cnt += tmp % 2;
-				even_cnt += tmp / 2;
-			}
-			
-			if(odd_cnt == 0 && even_cnt == 0) ans = 0;
-			else if(even_cnt > odd_cnt){
-				
-				while (even_cnt > odd_cnt + 1) {
-					even_cnt--;
-					odd_cnt += 2;
-					
-					//처음에 이렇게 하고 왜 50개 중 46개만 맞나 했는데 차이가 1이거나 0일때 결과가 다르게 나옵니다
-//					odd_cnt += 2;
-//					even_cnt -= 1;
-//					
-//					if(even_cnt - odd_cnt < 0) {
-//						even_cnt += 1;
-//						ans = even_cnt * 2;
-//
-//						break;
-//					}
-				}
-				
-				if (odd_cnt > even_cnt) {
-					ans = odd_cnt * 2 - 1;
-				} else if(even_cnt > odd_cnt) {
-					ans = even_cnt * 2;
-				} else {
-					ans = even_cnt * 2;
-				}
-				
+class Solution {
 
-			} else if(odd_cnt > even_cnt){
-				ans = odd_cnt * 2 - 1;
-			} else {
-				ans = even_cnt * 2;
-			}
-			
+    public static void main(String[] args) throws IOException {
 
-			
-			System.out.println("#" + test_case + " " + ans);
-		}
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        for(int t = 1; t <= T; t++) {
+            int N = Integer.parseInt(br.readLine());
+
+            int[] trees = new int[N];
+            int max_height = 0;
+
+            st = new StringTokenizer(br.readLine());
+            for(int i = 0; i < N; i++) {
+                trees[i] = Integer.parseInt(st.nextToken());
+                max_height = Math.max(max_height, trees[i]);
+            }
+
+            int odd = 0;
+            int even = 0;
+            
+            for(int i = 0; i < N; i++) {
+                int diff = max_height - trees[i];
+                if (diff == 0) continue;
+                
+                even += diff / 2;
+                odd += diff % 2;
+            }
+            
+            if(even > odd) {
+                while(Math.abs(even - odd) > 1) {
+                    even--;
+                    odd += 2;
+                }
+            }
+            
+            int ans = 0;
+            if (odd > even) {
+                ans = odd * 2 - 1;
+            } else if(even > odd) {
+                ans =even * 2;
+            } else {
+                ans = odd + even;
+            }
+
+            System.out.println("#" + t + " " + ans);
+        }
+
+    }
 }
