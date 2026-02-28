27a2c161feba047c4fb71d02ad0a297d5a80a8f1
한도윤 <92493882+semin98@users.noreply.github.com>
2026-02-28 13:47:19 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 111 ms, Memory: 33,920 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -4,78 +4,72 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.Deque;
-import java.util.StringTokenizer;
+import java.util.Queue;
 
-public class Solution {
+class Solution {
 
     static int N;
     static int[] di = {1, 0, -1, 0};
     static int[] dj = {0, 1, 0, -1};
-    static int[][] arr, v;
+    static int[][] map, war;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int T = Integer.parseInt(br.readLine());
-        StringTokenizer st;
 
-        for(int test_case = 1; test_case <= T; test_case++) {
+        for(int t = 1; t <= T; t++) {
 
             N = Integer.parseInt(br.readLine());
 
-            arr = new int[N][N];
-            v = new int[N][N];
+            map = new int[N][N];
+            war = new int[N][N];
 
             for(int i = 0; i < N; i++) {
-                char[] input = br.readLine().toCharArray();
+                Arrays.fill(war[i], Integer.MAX_VALUE);
+            }
+
+            war[0][0] = 0;
+
+            for(int i = 0; i < N; i++) {
+                String input = br.readLine();
                 for(int j = 0; j < N; j++) {
-                    arr[i][j] = input[j] - '0';
+                    map[i][j] = input.charAt(j) - '0';
+
                 }
             }
 
             bfs();
-
-            System.out.println("#" + test_case + " " + v[N - 1][N - 1]);
+            System.out.println("#" + t + " " + war[N - 1][N - 1]);
         }
 
     }
 
     static void bfs() {
-
-        for(int i = 0; i < N; i++) {
-            Arrays.fill(v[i], Integer.MAX_VALUE);
-        }
-
-        Deque<int[]> q = new ArrayDeque<>();
+        Queue<int[]> q = new ArrayDeque<>();
         q.offer(new int[] {0, 0});
-        v[0][0] = 0;
 
         while(!q.isEmpty()) {
-
-            int[] tmp = q.pollFirst();
-            int ci = tmp[0];
-            int cj = tmp[1];
+            int[] cur = q.poll();
+            int ci = cur[0];
+            int cj = cur[1];
+            
 
             for(int d = 0; d < 4; d++) {
                 int ni = ci + di[d];
                 int nj = cj + dj[d];
 
                 if(isIn(ni, nj)) {
-
-                    if(v[ni][nj] > v[ci][cj] + arr[ni][nj]) {
-                        v[ni][nj] = v[ci][cj] + arr[ni][nj];
+                    if(war[ci][cj] + map[ni][nj] < war[ni][nj]) {
+                        war[ni][nj] = war[ci][cj] + map[ni][nj];
                         q.offer(new int[] {ni, nj});
                     }
                 }
             }
-
         }
     }
 
-    static boolean isIn(int ci, int cj) {
-        if(0 <= ci && ci < N && 0 <= cj && cj < N) return true;
-        return false;
+    static boolean isIn(int si, int sj) {
+        return 0 <= si && si < N && 0 <= sj && sj < N;
     }
 }
