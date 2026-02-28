a4c2e170311764762c2dc13fb12a9aa80848a19c
한도윤 <92493882+semin98@users.noreply.github.com>
2026-02-28 14:49:03 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,5 +1,4 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,64 +8,61 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, M, ans;
+    static int N, M;
     static int[] di = {1, 0, -1, 0};
     static int[] dj = {0, 1, 0, -1};
-    static boolean[][] chk;
+    static boolean[][] v;
     static int[][] map;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        ans = 0;
 
         map = new int[N][M];
-        chk = new boolean[N][M];
+        v = new boolean[N][M];
 
         for(int i = 0; i < N; i++) {
-            char[] tmp = br.readLine().toCharArray();
+            String input = br.readLine();
             for(int j = 0; j < M; j++) {
-                map[i][j] = tmp[j] - '0';
+                map[i][j] = input.charAt(j) - '0';
             }
         }
 
-        bfs();
-        System.out.println(ans);
-
+        System.out.println(bfs());
     }
 
-    static void bfs() {
+    static int bfs() {
         Queue<int[]> q = new ArrayDeque<>();
         q.offer(new int[] {0, 0, 1});
-        chk[0][0] = true;
+        v[0][0] = true;
 
         while(!q.isEmpty()) {
-            int[] tmp = q.poll();
-            int ci = tmp[0];
-            int cj = tmp[1];
-            int c = tmp[2];
+            int[] cur = q.poll();
+            int ci = cur[0];
+            int cj = cur[1];
+            int len = cur[2];
 
-            for (int d = 0; d < 4; d++) {
+            if(ci == N - 1 && cj == M - 1) {
+                return len;
+            }
+
+            for(int d = 0; d < 4; d++) {
                 int ni = ci + di[d];
                 int nj = cj + dj[d];
 
-                if(isIn(ni,nj) && !chk[ni][nj] && map[ni][nj] == 1){
-                    if(ni == N - 1 && nj == M - 1) {
-                        ans = c + 1;
-                        return;
-                    } else {
-                        chk[ni][nj] = true;
-                        q.offer(new int[] {ni, nj, c + 1});
-                    }
+                if(isIn(ni, nj) && !v[ni][nj] && map[ni][nj] == 1) {
+                    v[ni][nj] = true;
+                    q.offer(new int[] {ni, nj, len + 1});
                 }
             }
         }
+        return -1;
     }
 
-    static boolean isIn(int ci, int cj) {
-        if(0 <= ci && ci < N && 0 <= cj && cj < M) return true;
-        return false;
+    static boolean isIn(int si, int sj) {
+        return 0 <= si && si < N && 0 <= sj && sj < M;
     }
 }
