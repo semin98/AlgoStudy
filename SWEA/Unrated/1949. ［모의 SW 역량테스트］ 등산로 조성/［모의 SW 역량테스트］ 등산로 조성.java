ce3a1cbe32665f23e061b84326c848af15efc825
한도윤 <92493882+semin98@users.noreply.github.com>
2026-02-28 15:41:30 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 91 ms, Memory: 26,880 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -1,94 +1,82 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Solution {
+class Solution {
 
-    static int N, K, max, ans;
+    static int N, K, ans;
+    static int[] di = {1, 0, -1, 0};
+    static int[] dj = {0, 1, 0, -1};
     static int[][] map;
-    static boolean[][] visited;
-    static ArrayList<int[]> list;
-    static int[] di = {0, 1, 0, -1};
-    static int[] dj = {1, 0, -1, 0};
+    static boolean[][] v;
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
-        StringTokenizer st;
+        for(int t = 1; t <= T; t++) {
 
-        for(int test_case = 1; test_case <= T; test_case++) {
-
-            st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken()); // 배열 크기
-            K = Integer.parseInt(st.nextToken()); // 팔 수 있는 최대 깊이
-            map = new int[N][N];
-            visited = new boolean[N][N];
-            max = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
             ans = 0;
-            list = new ArrayList<>();
+
+            map = new int[N][N];
+            v = new boolean[N][N];
+
+            int max_height = 0;
 
             for(int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
                 for(int j = 0; j < N; j++) {
                     map[i][j] = Integer.parseInt(st.nextToken());
-                    if(map[i][j] > max) {
-                        max = map[i][j];
-                    }
+                    max_height = Math.max(max_height, map[i][j]);
                 }
             }
 
             for(int i = 0; i < N; i++) {
                 for(int j = 0; j < N; j++) {
-                    if(map[i][j] == max) {
-                        list.add(new int[] {i, j});
+                    if(map[i][j] == max_height) {
+                        v[i][j] = true;
+                        dfs(i, j, max_height, 1, 1);
+                        v[i][j] = false;
                     }
                 }
             }
+            System.out.println("#" + t + " " + ans);
 
-            for (int[] start : list) {
-                visited[start[0]][start[1]] = true;
-                dfs(start[0], start[1], 1, false);
-                visited[start[0]][start[1]] = false;
-            }
-
-            System.out.println("#" + test_case + " " + ans);
         }
     }
 
-    static void dfs(int si, int sj, int length, boolean bomb) {
+    static void dfs(int si, int sj, int height, int chance,  int len) {
 
-        ans = Math.max(ans, length);
+        ans = Math.max(ans, len);
 
         for(int d = 0; d < 4; d++) {
             int ni = si + di[d];
             int nj = sj + dj[d];
 
-            if(!isIn(ni, nj) || visited[ni][nj]) continue;;
-
-            if(map[ni][nj] < map[si][sj]) {
-                visited[ni][nj] = true;
-                dfs(ni, nj, length + 1, bomb);
-                visited[ni][nj] = false;
-            } else if(!bomb) {
-                if(map[ni][nj] - K < map[si][sj]) {
-                    int height = map[ni][nj];
-                    map[ni][nj] = map[si][sj] - 1;
-                    visited[ni][nj] = true;
-                    dfs(ni, nj, length + 1, true);
-                    visited[ni][nj] = false;
-                    map[ni][nj] = height;
+            if(isIn(ni, nj)) {
+                if(map[ni][nj] < height && !v[ni][nj]) {
+                    v[ni][nj] = true;
+                    dfs(ni, nj, map[ni][nj], chance, len + 1);
+                    v[ni][nj] = false;
+                } else if(map[ni][nj] >= height && chance > 0) {
+                    int h_diff = map[ni][nj] - height;
+                    if(h_diff + 1 <= K && !v[ni][nj]) {
+                        v[ni][nj] = true;
+                        dfs(ni, nj, height - 1, 0, len + 1);
+                        v[ni][nj] = false;
+                    }
                 }
             }
         }
     }
 
-    static boolean isIn(int ci, int cj) {
-
-        if(0 > ci || 0 > cj || ci >= N || cj >= N) return false;
-        return true;
+    static boolean isIn(int si, int sj) {
+        return 0 <= si && si < N && 0 <= sj && sj < N;
     }
 }
