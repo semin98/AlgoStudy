346d49bb084918a7a277786868fdefa5923b840c
한도윤 <92493882+semin98@users.noreply.github.com>
2026-03-05 22:35:04 +0900

    [Gold IV] Title: 트리의 지름, Time: 196 ms, Memory: 23128 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -1,77 +1,67 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
-class Node {
-	int to;
-	int cost;
-	
-	public Node (int to, int cost) {
-		this.to = to;
-		this.cost = cost;
-	}
-}
-
 public class Main {
-	
-	static ArrayList<Node>[] edges;
-	static boolean[] visited;
-	static int x;
-	static int value;
-	
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+
+    static class Edge {
+        int to, val;
         
-        int N = Integer.parseInt(br.readLine());
-        
-        if (N == 1) {
-        	System.out.println(0);
-        	System.exit(0);
+        Edge(int to, int val) {
+            this.to = to;
+            this.val = val;
         }
-        
-        edges = new ArrayList[N + 1];
-        
-        for (int i = 1; i <= N; i++) {
-        	edges[i] = new ArrayList<>();
-        }
-        
-        for (int i = 1; i < N; i++) {
-        	st = new StringTokenizer(br.readLine());
-        	
-        	int a = Integer.parseInt(st.nextToken());
-        	int b = Integer.parseInt(st.nextToken());
-        	int c = Integer.parseInt(st.nextToken());
-        	
-        	edges[a].add(new Node(b, c));
-        	edges[b].add(new Node(a, c));
-        }
-        
-        visited = new boolean[N + 1];
-        visited[1] = true;
-        
-        dfs(1, 0);
-        
-        visited = new boolean[N + 1];
-        visited[x] = true;
-        
-        dfs(x, 0);
-        
-        System.out.println(value);
     }
     
-    static void dfs(int node, int distance) {
-    	if (distance > value) {
-    		x = node;
-    		value = distance;
-    	}
-    	
-    	for (Node next : edges[node]) {
-    		if (visited[next.to]) continue;
-    		
-    		visited[next.to] = true;
-    		dfs(next.to, distance + next.cost);
-    	}
+    static List<Edge>[] adj;
+    static int maxDia = 0;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        if(n == 1) {
+            System.out.println(0);
+            return;
+        }
+        
+        adj = new ArrayList[n + 1];
+        for(int i = 1; i <= n; i++) adj[i] = new ArrayList<>();
+        
+        for(int i = 0; i < n - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int parent = Integer.parseInt(st.nextToken());
+            int child = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+            adj[parent].add(new Edge(child, value));
+        }
+        
+        dfs(1);
+
+        System.out.println(maxDia);
+    }
+    
+    static int dfs(int cur) {
+        int firstMax = 0;
+        int secondMax = 0;
+        
+        for(Edge next : adj[cur]) {
+            int len = dfs(next.to) + next.val;
+            
+            if(len > firstMax) {
+                secondMax = firstMax;
+                firstMax = len;
+            } else if (len > secondMax) {
+                secondMax = len;
+            }
+        }
+        
+        maxDia = Math.max(maxDia, firstMax + secondMax);
+        
+        return firstMax;
     }
 }
