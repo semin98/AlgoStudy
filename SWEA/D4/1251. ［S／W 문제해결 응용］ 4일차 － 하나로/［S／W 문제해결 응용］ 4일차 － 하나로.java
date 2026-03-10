b5b7794753e7a463027ab5ace5c3a11e723d2bac
한도윤 <92493882+semin98@users.noreply.github.com>
2026-03-10 14:12:32 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 983 ms, Memory: 94,580 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,85 +1,118 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-class Solution {
+public class Solution {
 
-    static int N;
-    static long[] X, Y;
-    static double E;
+	static class Node implements Comparable<Node>{
+		long start;
+		long end;
+		long weight;
+		
+		Node(long start, long end, long weight) {
+			this.start = start;
+			this.end = end;
+			this.weight = weight;
+		}
 
-    static class Edge implements Comparable<Edge> {
-        int to;
-        long distSq;
-
-        public Edge(int to, long distSq) {
-            this.to = to;
-            this.distSq = distSq;
-        }
-
-        @Override
-        public int compareTo(Edge o) {
-            return Long.compare(this.distSq, o.distSq);
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-
-        for(int t = 1; t <= T; t++) {
-
-            N = Integer.parseInt(br.readLine());
-            X = new long[N];
-            Y = new long[N];
-
-            StringTokenizer stX = new StringTokenizer(br.readLine());
-            StringTokenizer stY = new StringTokenizer(br.readLine());
-
-            for(int i = 0; i < N; i++) {
-                X[i] = Long.parseLong(stX.nextToken());
-                Y[i] = Long.parseLong(stY.nextToken());
-            }
-
-            E = Double.parseDouble(br.readLine());
-
-            System.out.println("#" +  t + " " + Math.round(prim() * E));
-        }
-
-    }
-
-    static double prim() {
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-
-        boolean[] v = new boolean[N];
-        double total = 0;
-        int cnt = 0;
-
-        pq.offer(new Edge(0 , 0));
-
-        while(!pq.isEmpty()) {
-            Edge cur = pq.poll();
-
-            if(v[cur.to]) continue;
-
-            v[cur.to] = true;
-            total += cur.distSq;
-            if(++cnt == N) break;
-
-            for(int i = 0; i < N; i++) {
-                if(!v[i]) {
-                    long d = getDist(cur.to, i);
-                    pq.offer(new Edge(i, d));
-                }
-            }
-        }
-        return total;
-    }
-
-    static long getDist(int i, int j) {
-        return (X[i] - X[j]) * (X[i] - X[j]) + (Y[i] - Y[j]) * (Y[i] - Y[j]);
-    }
+		@Override
+		public int compareTo(Node o) {
+			return Long.compare(this.weight, o.weight);
+		}
+		
+	}
+	
+	static void makeSets() {
+		parents = new long[N];
+		
+		for(int i = 0; i < N; i++) {
+			parents[i] = i;
+		}
+	}
+	
+	static long findSet(long a) {
+		
+		if(a == parents[(int) a]) return a;
+		
+		return parents[(int)a] = findSet(parents[(int) a]);
+	}
+	
+	static boolean union(long a, long b) {
+		int aRoot = (int) findSet(a);
+		int bRoot = (int) findSet(b);
+		
+		if(aRoot == bRoot) return false;
+		
+		parents[bRoot] = aRoot;
+		return true;
+	}
+	
+	static int N;
+	static long[][] pos;
+	static long[] parents;
+	static Node[] nodes;
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t = 1; t <= T; t++) {
+			
+			N = Integer.parseInt(br.readLine());
+			
+			pos = new long[N][2];
+			makeSets();
+		
+			int cnt = 0;
+			int tmp = N - 1;
+			
+			while(tmp > 0) {
+				cnt += tmp--;
+			}
+			
+			nodes = new Node[cnt];
+			
+			StringTokenizer str = new StringTokenizer(br.readLine());
+			StringTokenizer stc = new StringTokenizer(br.readLine());
+			
+			for(int i = 0; i < N; i++) {
+				pos[i][0] = Integer.parseInt(str.nextToken());
+				pos[i][1] = Integer.parseInt(stc.nextToken());
+			}
+			
+			double E = Double.parseDouble(br.readLine());
+			
+			cnt = 0;
+			for(int i = 0; i < N - 1; i++) {
+				for(int j = i + 1; j < N; j++) {	
+					long dist = getDistance(pos[i][0], pos[i][1], pos[j][0], pos[j][1]);
+					nodes[cnt++] = new Node(i, j, dist);
+				}
+			}
+			
+			Arrays.sort(nodes);
+			
+			cnt = 0;
+			long result = 0;
+			for(Node node : nodes) {
+				if(union(node.start, node.end)) {
+					result += node.weight;
+					if(++cnt == N - 1) break;
+				}
+			}
+			
+			System.out.println("#" + t + " " +  (long)(Math.round(result * E)));
+		}
+		
+	}
+	
+	static long getDistance(long pos2, long pos3, long pos4, long pos5) {
+		return (long)(pos2 - pos4) * (pos2 - pos4) + (long)(pos3 - pos5) * (pos3 - pos5);
+	}
 }
+
