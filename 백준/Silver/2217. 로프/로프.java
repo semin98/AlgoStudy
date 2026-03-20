7de59fd41f9b40072045ac67178a72a2ec0407fd
한도윤 <92493882+semin98@users.noreply.github.com>
2026-03-20 17:31:49 +0900

    [Silver IV] Title: 로프, Time: 268 ms, Memory: 26556 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		int ans = 0;
+		for(int i = 0; i < N; i++) {
+			int tmp = arr[i] * (N - i);
+			ans = Math.max(ans, tmp);
+		}
+		
+		System.out.println(ans);
+	}
+	
+}
